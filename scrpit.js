7bd65a3a0d4414500843d42bb06fd69091c050b0
unknown <luisantoniobatistapereira@gmail.com>
2022-01-31 15:55:10 -0300

    Adicionando um reqres

---
+++ scrpit.js
@@ -0,0 +1,24 @@
+const init = () => {
+    const inputEmail = document.querySelector('input[type="email"]')
+    const inputPassword = document.querySelector('input[type="password"]')
+    const submitButton = document.querySelector('.login_submit')
+
+    if(submitButton){
+        submitButton.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            fetch('https://reqres.in/api/login', {
+                method: 'POST',
+                headers: {
+                    'content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: inputEmail.value,
+                    password: inputPassword.value
+                })
+            }).then((res) => { return res.json(); }).then((data) => { console.log(data)})
+        })
+    }
+}
+
+window.onload = init
